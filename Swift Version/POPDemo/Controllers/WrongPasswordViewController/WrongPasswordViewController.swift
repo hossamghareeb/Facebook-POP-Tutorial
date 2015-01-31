10fbc5278c12aa8b787ddf8c1af9164ba4aa82db
Hossam Ghareeb <hossam.ghareb@gmail.com>
2015-01-31 22:59:12 +0200

    Swift version of text field animation

---
+++ Swift Version/POPDemo/Controllers/WrongPasswordViewController/WrongPasswordViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WrongPasswordViewController.swift
+//  POPSwiftDemo
+//
+//  Created by Hossam Ghareeb on 1/31/15.
+//  Copyright (c) 2015 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class WrongPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
